197635aa848889def2a3958bccad10526e814330
Keegan Farley <keegomyneego@live.com>
2015-09-07 01:16:16 -0700

    testing changes to codepen

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 
 	$(document).ready(function () {
 		$("body").load("views/cascadeView.html", function () {
+            console.log("loaded view");
 			$.get("/scripts/views/cascadeViewModel.js").done(function () {
+                console.log("loaded view model");
 				ko.applyBindings();
 			});
 		});
